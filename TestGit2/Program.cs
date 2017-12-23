bee4ae1d0a14fb12a125cc1272bd9aa3a8d1e98c
ckenyeres <ckenyeres@gmail.com>
2017-12-23 00:49:58 -0500

    * TestGit2.sln: test
    * Program.cs:
    * TestGit2.csproj:

---
+++ TestGit2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestGit2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
